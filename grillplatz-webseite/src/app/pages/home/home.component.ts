96be6f9e12e7cbb5c24ab2169524bcd50d5a98d7
Raul Kirsch <raul.kirsch0207@gmail.com>
2025-05-01 16:31:10 +0200

    changed carousel

--- grillplatz-webseite/src/app/pages/home/home.component.ts
+++ grillplatz-webseite/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { OfferGalleryComponent } from '@app/components/offer-gallery/offer-gallery.component';
-import { NavbarComponent } from '@app/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [OfferGalleryComponent, NavbarComponent],
-  template: `<app-offer-gallery></app-offer-gallery>`,
+  imports: [OfferGalleryComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+
 })
 export class HomeComponent {}
