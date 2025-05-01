09eb5058a9edda837a06e40dd2826cfbe94df5a5
Raul Kirsch <raul.kirsch0207@gmail.com>
2025-05-01 16:46:40 +0200

    final carousel

--- grillplatz-webseite/src/app/components/offer-gallery/offer-gallery.component.ts
+++ grillplatz-webseite/src/app/components/offer-gallery/offer-gallery.component.ts
@@ -11,14 +11,18 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./offer-gallery.component.css']
 })
 export class OfferGalleryComponent {
-  activeIndex: number= 0;
+  activeIndex: number = 0;
 
   offers = [
     { id: '1', title: 'Musikanlage', imageUrl: '/assets/musikanlage.jpg', price: 60 },
-    { id: '2', title: 'Sportbox', imageUrl: '/assets/sportbox.jpg', price: 20},
-    { id: '3', title: 'Dartscheibe', imageUrl: '/assets/dartscheibe.jpg', price: 15},
-    { id: '4', title: 'Tischkicker', imageUrl: '/assets/tischkicker.jpg', price: 20},
-    { id: '5', title: 'Tischtennisplatte', imageUrl: '/assets/tischtennis.jpg', price: 25 },
+    { id: '2', title: 'Sportbox', imageUrl: '/assets/sportbox.jpg', price: 20 },
+    { id: '3', title: 'Dartscheibe', imageUrl: '/assets/dartscheibe.jpg', price: 15 },
+    { id: '4', title: 'Tischkicker', imageUrl: '/assets/tischkicker.jpg', price: 20 },
+    { id: '5', title: 'Tischtennisplatte', imageUrl: '/assets/tischtennis.jpg', price: 20 },
     { id: '6', title: 'Lichter / LED', imageUrl: '/assets/lichter.jpg', price: 10 }
   ];
+
+  onSlideChange(event: any) {
+    this.activeIndex = event.page;
+  }
 }
