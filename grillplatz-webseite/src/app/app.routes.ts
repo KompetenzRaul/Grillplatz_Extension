15dc629021aa6265b94dda579865ce96595600e0
Raul Kirsch <raul.kirsch0207@gmail.com>
2025-05-02 20:40:17 +0200

    back to prerender

--- grillplatz-webseite/src/app/app.routes.ts
+++ grillplatz-webseite/src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
   { path: 'kontakt', component: KontaktComponent },
   { path: 'impressum', component: ImpressumComponent },
   { path: 'datenschutz', component: DatenschutzComponent },
-  {path : 'produkte/:id', component: ProduktDetailComponent, data: { renderMode: 'client' } },
+  {path : 'produkte/:id', component: ProduktDetailComponent,},
   {path : 'anfrage', component: AuswahlComponent},
 
 ];
