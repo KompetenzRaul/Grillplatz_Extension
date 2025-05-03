e4db4282a3dff275bd101c80a022802f154dbd3c
Raul Kirsch <raul.kirsch0207@gmail.com>
2025-05-03 12:56:51 +0200

    change product site

--- grillplatz-webseite/src/app/pages/angebote/angebote.component.ts
+++ grillplatz-webseite/src/app/pages/angebote/angebote.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { OfferGalleryComponent } from 'src/app/components/offer-gallery/offer-gallery.component';
-
+import { PRODUKTE } from 'src/app/data/product-data';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-angebote',
   standalone: true,
-  imports: [OfferGalleryComponent],
+  imports: [CommonModule,RouterModule],
   templateUrl: './angebote.component.html',
-  styleUrls: ['./angebote.component.css']
+  styleUrls: ['./angebote.component.css'],
 })
-export class AngeboteComponent {}
+export class AngeboteComponent {
+  offers = PRODUKTE;
+}
+// 
