490272cab885ccf828eac79aecb460cdaae0ce6a
Raul Kirsch <raul.kirsch0207@gmail.com>
2025-05-04 13:28:27 +0200

    fail

--- grillplatz-webseite/src/app/server/routes.ts
+++ grillplatz-webseite/src/app/server/routes.ts
@@ -1,6 +1,5 @@
-import { PrerenderRoute } from '@angular/core';
 
-export function getPrerenderRoutes(): PrerenderRoute[] {
+export function getPrerenderRoutes(): Array<{ path: string }> {
   const ids = ['musikanlage', 'laser', 'sportbox','riesen-jenga','darts', 'lichter']; // <- diese IDs musst du mit deinen echten Produkt-IDs ersetzen
   return ids.map(id => ({
     path: `/produkte/${id}`
