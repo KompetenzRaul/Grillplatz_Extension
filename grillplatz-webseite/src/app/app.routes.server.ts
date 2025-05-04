0bd64ecf2d621d3d2a5a8549741792a7adf5ff78
Raul Kirsch <raul.kirsch0207@gmail.com>
2025-05-04 13:37:55 +0200

    added getPrerenderParams to server routes

--- grillplatz-webseite/src/app/app.routes.server.ts
+++ grillplatz-webseite/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'produkte/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const ids = ['musikanlage', 'laser', 'sportbox', 'riesen-jenga', 'darts', 'lichter'];
+      return ids.map(id => ({ id }));
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
