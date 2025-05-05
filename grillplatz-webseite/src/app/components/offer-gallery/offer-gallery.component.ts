01a516372f0aaba9049f581d5a38eeaa72724758
Raul Kirsch <raul.kirsch0207@gmail.com>
2025-05-05 15:15:11 +0200

    changed img src

--- grillplatz-webseite/src/app/components/offer-gallery/offer-gallery.component.ts
+++ grillplatz-webseite/src/app/components/offer-gallery/offer-gallery.component.ts
@@ -15,16 +15,16 @@ export class OfferGalleryComponent implements AfterViewInit {
   activeIndex: number = 0;
 
   offers = [
-    { id: 'musikanlage', title: 'Musikanlage', imageUrl: '/assets/musikanlage.jpg', price: 60 },
-    { id: 'laser', title: 'Laser-Lichtanlage', imageUrl: '/assets/laser.jpg', price: 20 },
-    { id: 'sportbox', title: 'Sportbox', imageUrl: '/assets/sportbox.jpg', price: 20 },
-    { id: 'riesen-jenga', title: 'Riesen-Jenga', imageUrl: '/assets/riesen_jenga.jpg', price: 10 },
-    { id: 'darts', title: 'Darts', imageUrl: '/assets/dartscheibe.jpg', price: 20 },
-    { id: 'lichter', title: 'Lichter', imageUrl: '/assets/lichter.jpg', price: 20 },
+    { id: 'musikanlage', title: 'Musikanlage', imageUrl: 'assets/musikanlage.jpg', price: 60 },
+    { id: 'laser', title: 'Laser-Lichtanlage', imageUrl: 'assets/laser.jpg', price: 20 },
+    { id: 'sportbox', title: 'Sportbox', imageUrl: 'assets/sportbox.jpg', price: 20 },
+    { id: 'riesen-jenga', title: 'Riesen-Jenga', imageUrl: 'assets/riesen_jenga.jpg', price: 10 },
+    { id: 'darts', title: 'Darts', imageUrl: 'assets/dartscheibe.jpg', price: 20 },
+    { id: 'lichter', title: 'Lichter', imageUrl: 'assets/lichter.jpg', price: 20 },
     {
       id: 'paket-sport',
       title: '🏀 Sportpaket',
-      imageUrl: '/assets/sport_paket.jpg',
+      imageUrl: 'assets/sport_paket.jpg',
       price: 45,
       description: 'Enthält: Sportbox, Riesen-Jenga, Darts',
     },
@@ -32,13 +32,13 @@ export class OfferGalleryComponent implements AfterViewInit {
       id: 'paket-party',
       title: '🎉 Party-Paket',
       price: 65,
-      imageUrl: '/assets/party_paket.jpg',
+      imageUrl: 'assets/party_paket.jpg',
       description: 'Enthält: Musikanlage und Laser-Lichtanlage',
     },
     {
       id: 'paket-allin',
       title: '🔥 All-In Paket',
-      imageUrl: '/assets/paket_allin.jpeg',
+      imageUrl: 'assets/paket_allin.jpeg',
       price: 110,
       description: 'Alle verfügbaren Extras in einem Rundum-sorglos-Paket!',
     }
